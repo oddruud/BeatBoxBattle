7a8acbd1158d0fd8e75576785b27aa3de85d8434
Emiel Kwakkel <emiel.kwakkel@rabobank.nl>
2017-10-09 20:50:55 +0200

    🤓 Add readAnalog function

--- workshop/workshop.ino
+++ workshop/workshop.ino
@@ -11,14 +11,14 @@
 #define d7 7
 
 #define DEBUG 0
- 
+
 byte lastValueA0 = 0;
 byte currentValueA0;
-unsigned long lastDebounceTimeA0 = 0;  
+unsigned long lastDebounceTimeA0 = 0;
 
 byte lastValueA1 = 0;
 byte currentValueA1;
-unsigned long lastDebounceTimeA1 = 0;  
+unsigned long lastDebounceTimeA1 = 0;
 
 byte noteON = 144;//note on command
 unsigned long debounceDelay = 10;    // the debounce time; increase if the output flickers
@@ -30,7 +30,7 @@ void setup() {
 
     lastValueA0 = map(analogRead(a0), 0, 1023, 127, 0);
     lastValueA1 = map(analogRead(a1), 0, 900, 0, 127);
-    
+
     Serial.begin(9600);
 }
 
@@ -39,20 +39,45 @@ void setup() {
 int lastValuesD[N_DIGITAL_PINS] = {LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW};
 int currentValuesD[N_DIGITAL_PINS];
 int lastDebounceTimeD[N_DIGITAL_PINS];
+byte lastAnalogValue[] = {0,0,0,0};
+byte currentValue[] = {0,0,0,0};
+unsigned long lastDebounceTime[] = {0,0,0,0};
+
+void readAnalog(byte port) {
+  int reading = analogRead(port);
+  byte mappedValue = map(reading, 0, 1023, 0, 127); //map value to 127 - 0
+
+  if (mappedValue != lastAnalogValue[port]) {
+    // reset the debouncing timer
+    lastDebounceTime[port] = millis();
+  }
+
+  if ((millis() - lastDebounceTime[port]) > debounceDelayAnalog) {
+    // if the state has changed:
+    if (mappedValue != currentValue[port]) {
+      currentValue[port] = mappedValue;
+//        Serial.print("Potmeter : ");
+//        Serial.println(currentValueA0);
+      MIDImessage(noteON + port + 1, currentValue[port], 100);
+    }
+  }
+
+  lastAnalogValue[port] = mappedValue;
+}
 
 void readDigital(int pin, int channel) {
     int readingPin = digitalRead(pin);
-      
+
     if (readingPin != lastValuesD[pin]) {
       // reset the debouncing timer
       lastDebounceTimeD[pin] = millis();
     }
 
-    if ((millis() - lastDebounceTimeD[pin]) > debounceDelay) {  
+    if ((millis() - lastDebounceTimeD[pin]) > debounceDelay) {
       // if the state has changed:
       if (readingPin != currentValuesD[pin]) {
         currentValuesD[pin] = readingPin;
-  
+
         // only toggle the LED if the new button state is HIGH
         if (currentValuesD[pin] == HIGH) {
           if (DEBUG) {
@@ -67,56 +92,10 @@ void readDigital(int pin, int channel) {
 }
 
 void loop() {
-
-/** ANALOG INPUT A0 **/
-
-    int readingA0 = analogRead(a0);
-    byte mappedValueA0 = map(readingA0, 0, 1023, 127, 0); //map value to 127 - 0
-
-    if (mappedValueA0 != lastValueA0) {
-      // reset the debouncing timer
-      lastDebounceTimeA0 = millis();
-    }
-
-    if ((millis() - lastDebounceTimeA0) > debounceDelayAnalog) {  
-      // if the state has changed:
-      if (mappedValueA0 != currentValueA0) {
-        currentValueA0 = mappedValueA0;
-//        Serial.print("Potmeter : ");
-//        Serial.println(currentValueA0);
-        MIDImessage(noteON, currentValueA0, 100);
-      }
-    }
-
-    lastValueA0 = mappedValueA0;
-/** EOF A0 **/
+/** ANALOG INPUT **/
+  readAnalog(0);
 
 
-
-/** ANALOG INPUT A1 **/
-    int readingA1 = analogRead(a1);
-    byte mappedValueA1 = map(readingA1, 0, 900, 0, 127); //map value to 127 - 0. Dark -> 0 , Light -> 127
-
-    if (mappedValueA1 != lastValueA1) {
-      // reset the debouncing timer
-      lastDebounceTimeA1 = millis();
-    }
-
-    if ((millis() - lastDebounceTimeA1) > debounceDelayAnalog) {  
-      // if the state has changed:
-      if (mappedValueA1 != currentValueA1) {
-        currentValueA1 = mappedValueA1;
-//        Serial.print("Light : ");
-//        Serial.println(currentValueA1);
-        MIDImessage(noteON, currentValueA1, 100);
-      }
-    }
-
-    lastValueA1 = mappedValueA1;
-/** EOF A1 **/
-
-
-    
 /** DIGITAL INPUT D3 **/
   readDigital(d3, 1);
   readDigital(d4, 2);
